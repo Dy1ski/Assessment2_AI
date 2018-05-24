68b504e0a8661370d12d8714f6c915ec59a0b3bb
Dylan <21029693+Dy1ski@users.noreply.github.com>
2018-05-24 17:21:40 +1000

    Update main.java

--- AI_Assignment2/src/main.java
+++ AI_Assignment2/src/main.java
@@ -11,7 +11,45 @@ public class main
         if(args.length < 2)
         {
 
+            System.out.println("run.bat <FileName> <Method>");
+            System.out.println("*Please use the correct format*");
+
         }
+        else
+          {
+              String result = "";
+              readInTestFile r = new readInTestFile();
+              String method = args[1];
+              String[] fileContent = r.readProblem(args[0]);
+              List<tellAskSentence> recievedKB = r.getKB(fileContent[1]);
+              tellAskSentence receivedQuery = r.receivedQuery(fileContent[3]);
+              KnowledgeBase KB = new KnowledgeBase();
+              KB.tellComplete(receivedKB);
+              KB.tell(receivedQuery);
+            // getting info on information
+
+              FileInputStream in = new FileInputStream(args[0]);
+              // like tutorial
+              BufferedReader buff = new BufferedReader(new InputStreamReader(in));
+              // getting info out on screen
+// do a switch statement*************************************************
+              buff.readLine();
+              String tell = buff.readLine();
+              buff.readLine();
+              String ask = buff.readLine();
+              if(method.equals("BC"))
+              {
+                  bChaining backwardChain = new bChaining(ask, tell);
+                  System.out.println(backwardChain.Execution());
+              }
+              if(method.equals("FC"))
+              {
+                  fChaining forwardChain = new fChaining(ask, tell);
+                  System.out.print(forwardChain.execution());
+
+              }
+          }
+
 
     }
 }
